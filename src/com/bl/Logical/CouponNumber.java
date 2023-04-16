16d2942bd7db2e12e080d93144d688df5d039516
SADIQUE HUSSAIN <44770680+hsadiq@users.noreply.github.com>
2023-04-16 17:24:01 +0530

    Added Coupon Number Code to Assignment Day6

--- src/com/bl/Logical/CouponNumber.java
+++ src/com/bl/Logical/CouponNumber.java
@@ -1,2 +1,23 @@
-package com.bl.Logical;public class CouponNumber {
+package com.bl.Logical;
+
+import java.util.Scanner;
+
+public class CouponNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter Your Three Digit Coupon Number: ");
+
+        int n = scanner.nextInt();
+
+        int coupon = (int)(Math.random()*1000) % 999;
+        if (coupon==n){
+            System.out.println("Coupon Matched: "+n);
+            System.out.println("You Won!!");
+        }else{
+            System.out.println("Coupon Not Matched");
+            System.out.println("Lucky Coupon is: "+coupon);
+        }
+    }
 }
+//Added on git
